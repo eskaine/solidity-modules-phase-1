96c889d030d61dca4323130c92f14dcf48ac770b
Eskaine <eskaineliu@gmail.com>
2023-04-28 19:45:23 +0800

    refactor: simplified wallet connections

--- module-3/erc1155_with_frontend/client/src/layouts/Navbar.jsx
+++ module-3/erc1155_with_frontend/client/src/layouts/Navbar.jsx
@@ -1,12 +1,17 @@
 import Typography from "@mui/material/Typography";
-import Wallets from "@/features/Wallets";
 import styles from "@/styles/Navbar.module.css";
+import { grey } from "@mui/material/colors";
+import { Web3Button } from '@web3modal/react';
+
+const typoStyle = {
+    color: grey[700]
+};
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <Typography>ERC1155 Game Items</Typography>
-      <Wallets />
+      <Typography variant="h5" sx={typoStyle}>ERC1155 Game Items</Typography>
+      <Web3Button />
     </div>
   );
 }
--- module-3/erc1155_with_frontend/client/src/providers/WalletConnect.js
+++ module-3/erc1155_with_frontend/client/src/providers/WalletConnect.js
@@ -1,8 +1,4 @@
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
+import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { arbitrum, mainnet, polygon } from "wagmi/chains";
@@ -21,9 +17,7 @@ const ethereumClient = new EthereumClient(wagmiClient, chains);
 export default function WalletConnect({ children }) {
   return (
     <>
-      <WagmiConfig client={wagmiClient}>
-        {children}
-      </WagmiConfig>
+      <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </>
   );
