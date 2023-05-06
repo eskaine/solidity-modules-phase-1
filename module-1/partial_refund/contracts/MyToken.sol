8d2704d239e910175e5879bf938b4911387071d2
Eskaine <eskaineliu@gmail.com>
2023-05-06 15:13:18 +0800

    chore: added test for contract

--- module-1/partial_refund/contracts/MyToken.sol
+++ module-1/partial_refund/contracts/MyToken.sol
@@ -1,45 +1,36 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract MyToken is ERC20 {
-    address payable public owner;
-    bool private _isSaleAvailable;
-    uint256 public constant MAX_SUPPLY = 1000000000000000000000000;
-    uint256 public constant WEI_PER_TOKEN = 500000000000000;
-    uint256 public constant WEI_PER_SALE_TOKEN = 1000000000000000;
+    address payable public immutable owner;
+    uint256 public constant MAX_SUPPLY = 1e22;
+    uint256 public constant WEI_PER_TOKEN = 1e18;
+    uint256 public constant WEI_PER_SALE_TOKEN = 5e17;
 
-    event tokenPurchased(address buyer, uint256 tokenAmount);
-    event tokenSold(address seller, uint256 tokenAmount);
+    event TokenPurchased(address buyer, uint256 tokenAmount);
+    event TokenSold(address seller, uint256 tokenAmount);
 
     constructor() ERC20("MyToken", "MT") {
         owner = payable(msg.sender);
-        _isSaleAvailable = true;
     }
 
     function buyToken() public payable notOwner {
         // buyer needs to pay the minimum price of 1 token
         require(msg.value >= WEI_PER_SALE_TOKEN, "Not enough ether!");
-       
+
         uint256 currentSupply = totalSupply();
-        uint256 amount = (msg.value / WEI_PER_SALE_TOKEN) * (10 ** decimals());
+        uint256 amount = (10 ** decimals()) * (msg.value / WEI_PER_SALE_TOKEN);
+
         // revert if tokens are not available for minting
-        if((currentSupply + amount) > MAX_SUPPLY) {
+        if ((currentSupply + amount) > MAX_SUPPLY) {
             revert("Tokens not available!");
         }
 
-        // stop minting sales if minted supplies have reached
-        if(currentSupply == MAX_SUPPLY) {
-            _isSaleAvailable = false;
-        }
-
-        // buy from minting
-        if(_isSaleAvailable) {
-            _mint(msg.sender, amount);
-        }
-
-        emit tokenPurchased(msg.sender, amount);
+        _mint(msg.sender, amount);
+        emit TokenPurchased(msg.sender, amount);
     }
 
     function sellBack(uint256 amount) public notOwner {
@@ -48,24 +39,22 @@ contract MyToken is ERC20 {
         uint256 amountWorthInWei = amount * WEI_PER_TOKEN;
         uint256 amountBalance = balanceOf(seller);
 
-        if(amountWorthInWei > address(this).balance) {
+        if (amountWorthInWei > address(this).balance) {
             revert("Not enough funds!");
         }
 
-        if(amountInDecimals > amountBalance) {
+        if (amountInDecimals > amountBalance) {
             revert("Not enough balance!");
         }
 
         _burn(msg.sender, amountInDecimals);
-        if(totalSupply() < MAX_SUPPLY) {
-            _isSaleAvailable = true;
-        }
 
+        emit TokenSold(seller, amountInDecimals);
         seller.transfer(amountWorthInWei);
-        emit tokenSold(seller, amountInDecimals);
     }
 
     function withdraw(address payable to) public payable {
+        require(to != address(0), "Invalid address!");
         require(msg.sender == owner, "Not contract owner!");
         require(totalSupply() == 0, "Tokens are still in circulation!");
         uint256 amount = address(this).balance;
